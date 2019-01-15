c47dfcaa6dce4651c6d8351e8f750e461991d3ec
張閎傑 <dhec10701p@gmail.com>
2019-01-15 15:50:56 +0800

    added init file

--- setup.py
+++ setup.py
@@ -8,30 +8,31 @@ def rel(*parts):
     '''returns the relative path to a file wrt to the current directory'''
     return os.path.abspath(os.path.join(os.path.dirname(__file__), *parts))
 
+
 README = open('README.md', 'r').read()
 
 with open(rel('django_json_ld', '__init__.py')) as handler:
     INIT_PY = handler.read()
 
-
 VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
 
 setup(
-  name = 'django-json-ld',
-  packages = ['django_json_ld', 'django_json_ld/templatetags'],
-  version = VERSION,
-  description = 'Django template tag for json-ld',
-  long_description=README,
-  long_description_content_type="text/markdown",
-  author = 'Johnny Chang',
-  author_email = 'hiimdoublej.pi@gmail.com',
-  #  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{0}'.format(VERSION),
-  url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
-  keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
-  classifiers = [
-    'Programming Language :: Python :: 3.6',
-    'Framework :: Django',
-    'Environment :: Web Environment',
-    'License :: OSI Approved :: MIT License',
-  ],
+    name='django-json-ld',
+    packages=['django_json_ld'],
+    include_package_data=True,
+    version=VERSION,
+    description='Django template tag for json-ld',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Johnny Chang',
+    author_email='hiimdoublej.pi@gmail.com',
+    #  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{0}'.format(VERSION),
+    url='https://github.com/hiimdoublej/django-json-ld',  # use the URL to the github repo
+    keywords=['django', 'webpack', 'assets'],  # arbitrary keywords
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+    ],
 )

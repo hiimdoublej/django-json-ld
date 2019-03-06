a98bdd0cec35c36d6984cded140bf453dd510222
Dario Marcelino <dario@appscot.com>
2019-03-06 19:13:55 +0000

    compatibility with python<3.6

--- django_json_ld/templatetags/json_ld.py
+++ django_json_ld/templatetags/json_ld.py
@@ -9,5 +9,5 @@ register = template.Library()
 @register.simple_tag
 def render_json_ld(structured_data):
     dumped = json.dumps(structured_data, ensure_ascii=False)
-    text = f"<script type=application/ld+json>{dumped}</script>"
+    text = "<script type=application/ld+json>{dumped}</script>".format(dumped=dumped)
     return mark_safe(text)

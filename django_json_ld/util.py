5c5fe825cdd87353153d9a3a8acc95742f4aee31
Denis Roldán <denis@denisroldan.com>
2022-02-01 13:57:18 +0100

    fix(utils): Update force_text to force_str
    force_text() is deprecated in favor of force_str().
    This commit maked the library Django 4 compat.
    
    Closes https://github.com/hiimdoublej/django-json-ld/issues/24

--- django_json_ld/util.py
+++ django_json_ld/util.py
@@ -1,7 +1,7 @@
 import collections
 
 from django.utils.functional import Promise
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 from django.core.serializers.json import DjangoJSONEncoder
 
 
@@ -24,5 +24,5 @@ class LazyEncoder(DjangoJSONEncoder):
     """
     def default(self, obj):
         if isinstance(obj, Promise):
-            return force_text(obj)
+            return force_str(obj)
         return super(LazyEncoder, self).default(obj)
